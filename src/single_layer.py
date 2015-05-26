161f30f80a97480c1329bed11e5a014965097625
Vladimir Iglovikov <iglovikov@gmail.com>
2015-05-25 18:35:59 -0700

    Now single layer network takes images as 2D as an input.

--- src/single_layer.py
+++ src/single_layer.py
@@ -5,21 +5,29 @@ from nolearn.lasagne import NeuralNet
 from lasagne.nonlinearities import softmax
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-
+from sklearn.preprocessing import LabelEncoder
 __author__ = 'Vladimir Iglovikov'
 
 
 import pandas as pd
 
 train = pd.read_csv('../data/train.csv')
-test = pd.read_csv('../data/train.csv')
+# test = pd.read_csv('../data/test.csv')
 
 target = train['label']
 training = train.drop('label', 1)
 
-scaler = StandardScaler()
-X = scaler.fit_transform(training).astype(np.float32)
+# scaler = StandardScaler(with_mean=False)
+# X = scaler.fit_transform(training).astype(np.float32)
+# X = scaler.fit_transform(training.values).astype(np.float32)
+X = (training.values / 255.0).astype(np.float32)
 
+# X_test = (test.values / 255.0)
+np.random.shuffle(X)
+
+encoder = LabelEncoder()
+
+y = encoder.fit_transform(target.values).astype(np.int32)
 
 net1 = NeuralNet(
     layers=[  # three layers: one hidden layer
@@ -28,10 +36,10 @@ net1 = NeuralNet(
         ('output', layers.DenseLayer),
         ],
     # layer parameters:
-    input_shape=(None, 28 * 28),  # 28x28 input pixels per batch
-    hidden_num_units=100,  # number of units in hidden layer
+    input_shape=(None, 28, 28),  # 28x28 input pixels per batch
+    hidden_num_units=300,  # number of units in hidden layer
     output_nonlinearity=softmax,  # output layer uses identity function
-    output_num_units=1,  # 1 target values
+    output_num_units=10,  # 1 target values
 
     # optimization method:
     update=nesterov_momentum,
@@ -39,9 +47,11 @@ net1 = NeuralNet(
     update_momentum=0.9,
 
     regression=False,  # flag to indicate we're dealing with regression problem
-    max_epochs=400,  # we want to train this many epochs
+    max_epochs=10,  # we want to train this many epochs
     verbose=1,
     )
 
 
-net1.fit(X, target.astype(np.int32))
+X_reshaped = X.reshape(X.shape[0], 28, 28)
+
+net1.fit(X_reshaped, y)
